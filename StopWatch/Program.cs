be93e17c8cd9d12e2849b27bd3175c873f836221
mahmoudabdalmaksoud <mahmoudabdalmaksoud4@gmail.com>
2021-07-10 19:28:49 +0200

    Add files via upload

---
+++ StopWatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PStopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           StopWatch obj = new StopWatch();
+            while (true)
+            {
+                Console.WriteLine("1-Start a timer ");
+                Console.WriteLine("2-End the timer");
+                Console.WriteLine("3-exit");
+                String choice = Console.ReadLine();
+                if (choice == "1")
+                    obj.start();
+                if (choice == "2")
+                    Console.WriteLine(obj.end());
+                if (choice == "3")
+                    break;
+                    
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PStopWatch
+{
+    class StopWatch
+    {
+        private DateTime _tstart;
+       private Boolean _timmerRunning;
+        
+        public void start()
+        {
+            if (_timmerRunning == true)
+                throw new InvalidOperationException("A timmer is already running !");
+            else
+            {
+                _tstart = DateTime.Now;
+                _timmerRunning = true;
+            }
+            }
+
+
+        public TimeSpan end()
+        {
+            if (_timmerRunning == false)
+                throw new InvalidOperationException("there is no timer running");
+            else
+            {
+                _timmerRunning = false;
+                return DateTime.Now - _tstart;
+            }
+        }
+    }
+}
